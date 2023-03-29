32d32d8d2f482a24788d45545140876d316b1f78
Tim Honchel <tim.honchel@gmail.com>
2023-03-29 09:24:56 -0400

    added user table, user DTO, and insert user request

---
+++ ABCLearningPortal.DataAccess/DataRequests/InsertUser.cs
@@ -0,0 +1,31 @@
+﻿using ABCLearningPortal.DataAccess.Abstraction;
+using System;
+
+namespace ABCLearningPortal.DataAccess.DataRequests
+{
+    public class InsertUser : IDataRequest
+    {
+        public InsertUser(Guid guid, string firstName, string lastName, string userName)
+        {
+            Guid = guid;
+            FirstName = firstName;
+            LastName = lastName;
+            UserName = userName;
+        }
+
+        public Guid Guid { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string UserName { get; set; } = null!;
+
+        public object? GetParameters()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetSql()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ABCLearningPortal.DataAccess/DataTransferObjects/UserDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ABCLearningPortal.DataAccess.DataTransferObjects
+{
+    public class UserDTO
+    {
+        public int Id { get; set; }
+        public Guid Guid { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string UserName { get; set; } = null!;
+    }
+}
